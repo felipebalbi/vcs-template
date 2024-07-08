d7cc98405afb66c397edbce3af15b40ee128daa2
Felipe Balbi <felipe@balbi.sh>
2024-07-08 18:18:49 +0300

    draw a box with the playfield
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- src/main.s
+++ src/main.s
@@ -1,9 +1,6 @@
 	.include "atari2600.inc"
 	.export main
 	.import playfield_init
-	.import playfield_update
-	.import CURRENT_TIME
-	.import TIME_TO_CHANGE
 
 	;; The actual game code starts here. This is where we would setup an
 	;; intro screen, play sound and check for user input.
@@ -32,15 +29,46 @@ next_frame:
 
 	wsync #37		; 37 scanlines of Vertical Blank
 
-	jsr playfield_update	; update the playfield
-
 	;; Draw 192 visible scanlines
-	ldx #192
-loop_visible:
-	stx COLUBK
+	ldx #0
+
+	;; 8 scanlines of full playfield
+	lda #$ff
+	sta PF0
+	sta PF1
+	sta PF2
+
+top8lines:
 	sta WSYNC
-	dex
-	bne loop_visible
+	inx
+	cpx #8
+	bne top8lines
+
+	;; 176 scanlines of wall
+	lda #$10
+	sta PF0
+
+	lda #0
+	sta PF1
+	sta PF2
+
+middlelines:
+	sta WSYNC
+	inx
+	cpx #184
+	bne middlelines
+
+	;; 8 more scanlines of full playfield
+	lda #$ff
+	sta PF0
+	sta PF1
+	sta PF2
+
+bottom8lines:
+	sta WSYNC
+	inx
+	cpx #192
+	bne bottom8lines
 
 	lda #$42		; Enable I4/I5 latches; Enter Vertical Blank
 	sta VBLANK
--- src/playfield.s
+++ src/playfield.s
@@ -1,39 +1,13 @@
 	.include "atari2600.inc"
 	.export playfield_init
-	.export playfield_update
-	.export CURRENT_TIME
-	.export TIME_TO_CHANGE
-
-	.segment "BSS"
-PATTERN:	.res 1		; storage location
-CURRENT_TIME:	.res 1		; current time
-TIME_TO_CHANGE	= 20		; speed of animation
-
 
 	.segment "CODE"
 	.proc playfield_init
-	lda #0
-	sta PATTERN		; the binary playfield pattern
-
 	lda #$45
 	sta COLUPF		; set the playfield color
 
-	rts
-	.endproc
+	lda #1
+	sta CTRLPF		; enable playfield reflection
 
-	.segment "CODE"
-	.proc playfield_update
-	ldy CURRENT_TIME	; speed counter
-	iny			; increment speed count by one
-	cpy #TIME_TO_CHANGE	; has it reached our "change point"?
-	bne notyet		; no, so branch past
-
-	ldy #0			; reset speed count
-	inc PATTERN		; switch to next pattern
-
-notyet:
-	lda PATTERN		; use our saved pattern
-	sta PF1			; as the playfield shape
-	sty CURRENT_TIME
 	rts
 	.endproc
